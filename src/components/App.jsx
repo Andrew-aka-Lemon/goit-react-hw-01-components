58b7f34497995d72d05171b0ccd7601a70d311d2
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-11-18 00:00:31 +0200

    step5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    ></Profile>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+      <Statistics title="qweqwe" stats={data}></Statistics>
+    </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,28 @@
 import PropTypes from 'prop-types';
 
-export default function Statistics(props) {}
+export default function Statistics({ title, stats }) {
+  return (
+    <section clasName="statistics">
+      {title > 0 && <h2 clasName="title">{title}</h2>}
+      <ul clasName="stat-list">
+        {stats.map(stat => (
+          <li clasName="item" key={stat.id}>
+            <span clasName="label">{stat.label}</span>
+            <span clasName="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
-Statistics.propTypes = {};
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
