5f2b5785b7dfd6e77fad7eabe6e667099d6219d5
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-11-23 19:15:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import Profile from 'components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
-import user from '../user.json';
-import data from '../data.json';
+import user from 'user.json';
+import data from 'data.json';
 
 export const App = () => {
   return (
